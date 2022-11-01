06ff3d45a12fa8a20d0532d50bdedc9581c31b5b
KimHyunLo <leeas016@gmail.com>
2022-11-02 01:21:07 +0900

    work, project, youtube add

--- js/works.js
+++ js/works.js
@@ -2,7 +2,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 const pageContainer = document.querySelector("#container");
 
-/* SMOOTH SCROLL */
+// SMOOTH SCROLL
 const scroller = new LocomotiveScroll({
   el: pageContainer,
   smooth: true,
@@ -27,50 +27,27 @@ ScrollTrigger.scrollerProxy(pageContainer, {
   pinType: pageContainer.style.transform ? "transform" : "fixed",
 });
 
-// //스르륵 등장
-// const options = {
-//   root: null,
-//   rootMargin: "0px",
-//   threshold: 0.9,
-// };
+// CARD ENTER
+$(".card").mouseenter(function () {
+  gsap.fromTo(
+    $(this).siblings(".card__info"),
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      ease: Power1.easeInOut,
+    }
+  );
+});
 
-// let revealCallback = (entries, self) => {
-//   entries.forEach((entry) => {
-//     let container = entry.target;
-//     let img = entry.target.querySelector("li.card");
-//     const easeInOut = "power3.out";
-//     const revealAnim = gsap.timeline({ ease: easeInOut });
-
-//     if (entry.isIntersecting) {
-//       revealAnim.set(container, {
-//         visibility: "visible",
-//       });
-//       revealAnim.fromTo(
-//         container,
-//         {
-//           clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-//           webkitClipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
-//         },
-//         {
-//           clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-//           webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
-//           duration: 1,
-//           ease: easeInOut,
-//         }
-//       );
-//       revealAnim.from(img, 4, {
-//         scale: 1.4,
-//         ease: easeInOut,
-//         delay: -1,
-//       });
-//       self.unobserve(entry.target);
-//     }
-//   });
-// };
-
-// let revealObserver = new IntersectionObserver(revealCallback, options);
-
-// document.querySelectorAll("li.card").forEach((reveal) => {
-//   revealObserver.observe(reveal);
-// });
-                                                                                                                                       
+// CARD LEAVE
+$(".card").mouseleave(function () {
+  gsap.to($(this).siblings(".card__info"), {
+    y: -50,
+    opacity: 0,
+    ease: Power1.easeInOut,
+  });
+});
